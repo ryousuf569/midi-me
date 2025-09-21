6ec2665b4d7be29aae455e51020b274a80b9a8ed
Yousuf Muhammad Rashid <yousufrashid.coding@gmail.com>
2025-09-21 19:52:21 -0400

    made the output key specific based on user input

--- midi-data-extraction/ingest.py
+++ midi-data-extraction/ingest.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
+selected_key = input("Which key do you want? ")
+
 midi_repo_root = Path(__file__).parent.parent
-raw_midi_folder = midi_repo_root / "Data" / "MIDI FILES"/ "Raw"
+raw_midi_folder = midi_repo_root / "Data" / "MIDI FILES"/ "Raw" / selected_key
 midi_files = raw_midi_folder.rglob('*.mid')
 
 for midi in midi_files:
