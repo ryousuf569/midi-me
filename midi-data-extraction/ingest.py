a4b8fbaf2ccfa31681c996d6f8f01befa62072d2
ryousuf569569 <ryousuf569@gmail.com>
2025-09-21 21:25:32 -0400

    made it so it builds a list based off the key you want

--- midi-data-extraction/ingest.py
+++ midi-data-extraction/ingest.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+
 selected_key = input("Which key do you want? ")
 
 midi_repo_root = Path(__file__).parent.parent
 raw_midi_folder = midi_repo_root / "Data" / "MIDI FILES"/ "Raw" / selected_key
 midi_files = raw_midi_folder.rglob('*.mid')
 
-for midi in midi_files:
-    print(repr(midi.name))
+midi_list = [(repr(midi.name)) for midi in midi_files]
+print(midi_list)
+
