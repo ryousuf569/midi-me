ba24c4d3a436569d8a0afb3428714a204b905248
ryousuf569 <yousufrashid.coding@gmail.com>
2025-10-28 01:31:28 -0400

    added ticks_per_beat

--- scripts/midi_parser.py
+++ scripts/midi_parser.py
@@ -6,15 +6,19 @@ print("New Midi file created")
 
 def midi_data(midi_track):
     midi_data_list = list()
+    midi_data_note = list()
+    midi_data_time = list()
+    midi_data_vel = list()
     for i, track in enumerate(midi_track):
+        print(f"Track {i}: {track.name}")
         for msg in track:
             if ((list(str(msg))[0])=="n"):
-                midi_data_list.append(str(msg))
+                midi_data_list.append(msg.type)
+                midi_data_note.append(msg.note)
+                midi_data_time.append(msg.time)
+                midi_data_vel.append(msg.velocity)
             else:
                 pass
-    return midi_data_list
+    return midi_data_list, midi_data_note, midi_data_time, midi_data_vel
 
-note = md.Message.from_str(midi_data(midi.tracks)[0])
-print(note)
-print(note.note)
-print(note.time) 
+(print(midi.ticks_per_beat))
